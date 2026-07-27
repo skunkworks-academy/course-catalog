dd08299ee94279489a1b0c3002e818441bd54dab
Raydo M <126121348+burnt-exe@users.noreply.github.com>
2026-07-27 18:06:09 +0200

    Add course navigation

---
+++ sidebars.ts
@@ -0,0 +1,26 @@
+import type {SidebarsConfig} from '@docusaurus/plugin-content-docs';
+
+const sidebars: SidebarsConfig = {
+  courseSidebar: [
+    {
+      type: 'category',
+      label: 'Commerce administration',
+      collapsed: false,
+      items: ['shopify-user-permissions'],
+    },
+    {
+      type: 'category',
+      label: 'Web deployment',
+      collapsed: false,
+      items: ['github-pages-setup'],
+    },
+    {
+      type: 'category',
+      label: 'Microsoft 365 administration',
+      collapsed: false,
+      items: ['microsoft-365-licenses'],
+    },
+  ],
+};
+
+export default sidebars;
